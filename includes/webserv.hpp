734ceba8b2b4e4dca7bd75a13de00a024be25502
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-04-15 06:00:21 +0900

    [EDIT] alias 추가

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -4,6 +4,10 @@
 #define ERR_MSG_HAED "Error : "
 #define ERR_PARAM_MISSING "Usage : ./webserv [configuration file]"
 
-#define DEFAULT_CONFIGFILE_PATH "./config./default.config"
+#define DEFAULT_CONFIGFILE_PATH "./config/default.config"
+
+#define STRING_NPOS std::string::npos
+#define ERROR_END   1
+#define DONE        0
 
 #endif //WEBSERV_HPP
